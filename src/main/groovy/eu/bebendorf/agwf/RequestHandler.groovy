af1676bd81f588de4c8029fe75ff3dd1bd242bf3
JanHolger <jan@bebendorf.eu>
2020-04-20 13:49:58 +0200

    Removed RequestException and changed default not found

--- src/main/groovy/eu/bebendorf/agwf/RequestHandler.groovy
+++ src/main/groovy/eu/bebendorf/agwf/RequestHandler.groovy
@@ -4,7 +4,8 @@ interface RequestHandler {
     def handle(Exchange exchange)
     static class DefaultNotFoundHandler implements RequestHandler {
         def handle(Exchange exchange) {
-            throw new RequestException(404, "RESOURCE_NOT_FOUND")
+            exchange.status(404)
+            'Page not found!'
         }
     }
 }
