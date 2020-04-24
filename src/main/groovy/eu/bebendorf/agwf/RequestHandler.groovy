0eea386505825c5546198e9d72143fe08dcc690c
JanHolger <jan@bebendorf.eu>
2020-04-24 03:46:29 +0200

    Fixed exception handler

--- src/main/groovy/eu/bebendorf/agwf/RequestHandler.groovy
+++ src/main/groovy/eu/bebendorf/agwf/RequestHandler.groovy
@@ -1,7 +1,7 @@
 package eu.bebendorf.agwf
 
 interface RequestHandler {
-    def handle(Exchange exchange)
+    def handle(Exchange exchange) throws Throwable
     static class DefaultNotFoundHandler implements RequestHandler {
         def handle(Exchange exchange) {
             exchange.status(404)
