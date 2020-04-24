e61a4f5fd1c472bf25d53ca717a7be289fb6a229
JanHolger <jan@bebendorf.eu>
2020-04-24 03:03:37 +0200

    Fixed exception handler

--- src/main/groovy/eu/bebendorf/agwf/ExceptionHandler.groovy
+++ src/main/groovy/eu/bebendorf/agwf/ExceptionHandler.groovy
@@ -4,12 +4,12 @@ import java.nio.charset.StandardCharsets
 
 interface ExceptionHandler {
     def handle(Exchange exchange, Throwable ex);
+    default byte[] handleBytes(Exchange exchange, Throwable ex){
+        handle(exchange, ex).getBytes(StandardCharsets.UTF_8)
+    }
     static class DefaultExceptionHandler implements ExceptionHandler {
         String handle(Exchange exchange, Throwable ex) {
             'An internal server error occured! Please contact the server administrator in case you think this is a problem.'
-        }
-        default byte[] handleBytes(Exchange exchange, Throwable ex){
-            handle(exchange, ex).getBytes(StandardCharsets.UTF_8)
         }
     }
 }
